841a47da0447d9567578c87431e4ca0a29a1a6aa
Hannah Kim <48963450+hkim37@users.noreply.github.com>
2019-09-02 09:19:13 -0400

    Create printCommand.py

---
+++ printCommand.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import sys
+import os
+
+def printCommand(gene):
+    pre = '/net/data/acmg59/final/{}'.format(gene)
+    finalPaths = os.listdir(pre) # ['1-100_0.3-0.5','1-100_0.5-0.5',...]
+        
+    for path in finalPaths:
+        start = int((path.split('_')[0]).split('-')[0])
+        end = int((path.split('_')[0]).split('-')[-1])
+        bit = float((path.split('_')[-1]).split('-')[0])
+        
+        print('sbatch -o /n/scratch2/rg252/ACMG59_NEW/outFiles/{}_out.txt config.sh {} 0.5 {} {}'.format(gene,bit,start,end))
+
+def main():
+    gene = sys.argv[1]
+    print()
+    printCommand(gene)
+    print()
+    
+if __name__=="__main__":
+    main()
